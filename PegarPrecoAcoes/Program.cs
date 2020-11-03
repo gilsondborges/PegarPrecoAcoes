5d55a306c9cd4897a9a74eead7eb78ae839df17e
gilsondborges <73753368+gilsondborges@users.noreply.github.com>
2020-11-02 21:24:59 -0300

    Add files via upload

---
+++ PegarPrecoAcoes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace PegarPrecoAcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string json = webClient.DownloadString("https://query1.finance.yahoo.com/v8/finance/chart/PETR3.SA?period1=1598922000&period2=1604005200&interval=1d");
+                Console.WriteLine(json);
+                Console.ReadKey();
+            }
+        }
+    }
+}
